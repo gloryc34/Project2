b27773ba4f881f81dd359cff69ae1f548650ea8b
gloryc34 <114937071+gloryc34@users.noreply.github.com>
2023-04-18 11:10:15 -0400

    Update normalFunctionTester.java

--- Apache/Code/normalFunctionTester.java
+++ Apache/Code/normalFunctionTester.java
@@ -2,7 +2,7 @@ import org.jfree.ui.RefineryUtilities;
 /**
  * @author Colin Glory
  * @version Version: 2022-12 (4.26.0)
- * 
+ * Followed tutorial to graph from https://www.tutorialspoint.com/jfreechart/jfreechart_xy_chart.htm
  */
 public class normalFunctionTester {
 	/**
