0cc32c553e33617ddcd6010092104a73fa4aea0d
gloryc34 <114937071+gloryc34@users.noreply.github.com>
2023-04-18 11:10:51 -0400

    Update saltFunctionTester.java

--- Apache/Code/saltFunctionTester.java
+++ Apache/Code/saltFunctionTester.java
@@ -2,7 +2,8 @@ import org.jfree.ui.RefineryUtilities;
 /**
  * @author Colin Glory
  * @version Version: 2022-12 (4.26.0)
- * Ideas from: http://www.java2s.com/example/java-api/org/jfree/chart/renderer/xy/xylineandshaperenderer/xylineandshaperenderer-0-2.html
+ * Followed tutorial to graph from https://www.tutorialspoint.com/jfreechart/jfreechart_xy_chart.htm
+ * Generated ideas and got information from: https://commons.apache.org/proper/commons-math/  https://www.jfree.org/jfreechart/  https://www.tutorialspoint.com/jfreechart/jfreechart_xy_chart.htm  https://www.youtube.com/watch?v=MifbwoCumX0&t=402s
  */
 public class saltFunctionTester {
 	
